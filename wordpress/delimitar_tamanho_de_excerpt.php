4b2ebbfb76f18a0359405e2cf0efbbb6698b3cc4
jonathanslima <jonathan@cartello.com.br>
2017-08-01 12:44:43 -0300

    delimitar a quantidade de caracteres de um content

--- wordpress/delimitar_tamanho_de_excerpt.php
+++ wordpress/delimitar_tamanho_de_excerpt.php
@@ -1,8 +1,8 @@
  <?php
-	$content_sobre = get_the_content();
-	if ( strlen( $content_sobre ) > 90 ) { ?>
-	    <p class="text text-variant"><i><?php echo mb_strimwidth( $content_sobre, 0, 240, '...' ); ?></i></p>
+	$content = get_the_content();
+	if ( strlen( $content ) > 90 ) { ?>
+	    <p class="text text-variant"><i><?php echo mb_strimwidth( $content, 0, 240, '...' ); ?></i></p>
 	<?php } else { ?>
-	    <p><?php echo $content_sobre; ?></p>
+	    <p><?php echo $content; ?></p>
 	<?php } 
 ?>
